5761be559656edf3bd600e1c669e757cd4241e14
Michael_Fesenko <@gmail.com>
2023-07-08 12:51:13 +0300

    добавил функцию в Task_13

--- Task_13/Program.cs
+++ Task_13/Program.cs
@@ -4,6 +4,37 @@
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
+// Вариант 1. Без функции.
+// Console.Write("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// if (num / 100 < 1)
+// {
+//     Console.Write("Третьей цифры нет");
+// }
+// else
+// {
+//     int k = num;
+//     while (k / 1000 >= 1)
+//     {
+//         k = (k - k % 10) / 10;
+//     }
+//     int thirdDigit = k % 10;
+//     Console.Write($"Третья цифра числа {num}: {thirdDigit}");
+// }
+
+// Вариант 2. С функцией.
+int SearchThirdDigit(int k) // ищет третью цифру числа
+{
+    int thirdDigit = -1; // на случай ошибки
+    while (k / 1000 >= 1)
+    {
+        k = (k - k % 10) / 10;
+    }
+    thirdDigit = k % 10;
+    return thirdDigit;
+}
+
 Console.Write("Введите число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 
@@ -13,11 +44,5 @@ if (num / 100 < 1)
 }
 else
 {
-    int k = num;
-    while (k / 1000 >= 1)
-    {
-        k = (k - k % 10) / 10;
-    }
-    int thirdDigit = k % 10;
-    Console.Write($"Третья цифра числа {num}: {thirdDigit}");
+    Console.Write($"Третья цифра числа {num}: {SearchThirdDigit(num)}");
 }
